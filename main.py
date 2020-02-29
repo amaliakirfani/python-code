92dfd1114702650124e76db6140e0e08d0b6472d
amaliakirfani <ridwands@ridwands-latitude-e6410.win-eoj3gbj5a6m.pancacourse.id>
2020-02-29 14:51:27 +0700

    change python

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flaskext.mysql import MySQL
 from flask import request
 from flask import render_template, Flask
 from flask import jsonify
+import pymysql
 
 app = Flask(__name__)
 
@@ -16,26 +17,32 @@ mysql.init_app(app)
 @app.route('/')
 def index():
     return render_template('hello.html')
-    
-@app.route('/inputmahasiswa', methods=['POST'])
-def inputmahasiswa():
-    # nama = request.form['nama']
-    # nim = request.form['nim']
-    # tgl_lahir = request.form['tgl_lahir']
-    nim = request.json['nim']
-    nama = request.json['nama']
-    tgl_lahir = request.json['tgl_lahir']
-    
+
+@app.route('/absensi', methods=['POST'])
+def absensi():
+    rfid_id=request.json['rfid_id']
+
     conn = mysql.connect()
-    cursor = conn.cursor()
-    sql = "Insert into mahasiswa (nama,nim,tgl_lahir) values (%s,%s,%s)"
-    data = (nama,nim,tgl_lahir)
-    cursor.execute(sql,data)
-    conn.commit()
-    result = jsonify({ 
-        "Message" : "ID Succesfully Added"
-    })
-    return result
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+    cursor.execute("select * from mahasiswa where rfid_id=%s", rfid_id)
+    mahasiswa = cursor.fetchone() 
+
+    if mahasiswa is None:
+        sql = "Insert into mahasiswa (rfid_id) values (%s)"
+        data = (rfid_id)
+        cursor.execute(sql,data)
+        conn.commit()
+        res = jsonify({
+            "Message" : "ID Successfully Added"
+        })
+        return res
+    else:
+        res = jsonify({
+        "Message" : "ID Already There"
+        })
+        return res
+
 
 if __name__=="__main__":
-    app.run(port=2000, debug=True)
+    app.run(port=5000,host="172.16.18.246", debug=True)
